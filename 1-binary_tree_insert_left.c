77fcee030026cb90b620d1fb2f9287aa68f267ec
Albert Mpepo <albertmpepo@gmail.com>
2023-11-01 13:59:19 +0200

    base tasks

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - a function that inserts a node as the left-child of another node.
+ * @parent: a pointer to the parent node of the node to create.
+ * @value: the value to put in the new node.
+ *
+ * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - a function that inserts a node as the right-child of another node.
+ * @parent: a pointer to the parent node of the node to create.
+ * @value: the value to put in the new node.
+ *
+ * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
