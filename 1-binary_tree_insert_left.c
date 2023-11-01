e5b582e5854f3e25f3cb0ecceb2aa3ae2d3428b0
Albert Mpepo <albertmpepo@gmail.com>
2023-11-01 17:42:11 +0200

    1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,5 +9,21 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
 	
+	binary_tree_t *node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+	}
+	
+	parent->left = node;
+
+	return (node);
 }
