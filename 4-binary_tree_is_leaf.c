7f0e57b850212b4119205768cab4c91519ef8c48
Albert Mpepo <albertmpepo@gmail.com>
2023-11-01 22:01:20 +0200

    4. Is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,3 +7,12 @@
  * Return: 1 if node is a leaf, otherwise 0.
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->left == NULL)
+	{
+		if (node->right == NULL)
+			return (1);
+	}
+
+	return (0);
+}
