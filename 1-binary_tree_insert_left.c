5772a23e362a9707c6b807f40b85aded5884a386
Albert Mpepo <albertmpepo@gmail.com>
2023-11-01 23:19:56 +0200

    betty fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,18 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - a function that inserts a node as the left-child of another node.
+ * binary_tree_insert_left - a function that inserts a node as
+ * the left-child of another node.
  * @parent: a pointer to the parent node of the node to create.
  * @value: the value to put in the new node.
  *
- * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: a pointer to the created node, or NULL on failure or
+ * if parent is NULL
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
 		return (NULL);
-	
+
 	binary_tree_t *node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
 	if (node == NULL)
 		return (NULL);
 
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - a function that inserts a node as the right-child of another node.
+ * binary_tree_insert_right - a function that inserts a node as
+ * the right-child of another node.
  * @parent: a pointer to the parent node of the node to create.
  * @value: the value to put in the new node.
  *
- * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: a pointer to the created node, or NULL on failure or
+ * if parent is NULL
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -13,6 +15,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	
 	binary_tree_t *node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
 	if (node == NULL)
 		return (NULL);
 
