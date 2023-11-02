c5d335c23825faae77e54eecf9325e5cad3ea1f3
Albert Mpepo <albertmpepo@gmail.com>
2023-11-02 21:54:26 +0200

    betty fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - a function that measures the balance factor of a binary tree.
+ * binary_tree_balance - a function that measures
+ * the balance factor of a binary tree.
  * @tree: a pointer to the root node of the tree to measure the balance factor.
  *
  * Return: the balance factor of tree, 0 if tree is NULL.
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	
+
 }
