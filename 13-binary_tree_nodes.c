9938a55cd756a8a0590a7895477876bd0c39e944
Albert Mpepo <albertmpepo@gmail.com>
2023-11-02 03:45:10 +0200

    last base

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - a function that counts the nodes with
+ * at least 1 child in a binary tree.
+ * @tree: a pointer to the root node of the tree to count the number of nodes.
+ * 
+ * Return: the node of tree, 0 if tree is NULL.
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+}
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - a function that checks if a binary tree is full.
+ * @tree: a pointer to the root node of the tree to check.
+ * 
+ * Return: 1 if tree is full, 0 if tree is NULL.
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+
+}
