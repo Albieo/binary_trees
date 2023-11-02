868b3a8be6f6b653f4d9a709bf7177a1faecbfd7
Albert Mpepo <albertmpepo@gmail.com>
2023-11-02 21:35:10 +0200

    13. Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,6 +11,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	
+	if (tree->left || tree->right)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
