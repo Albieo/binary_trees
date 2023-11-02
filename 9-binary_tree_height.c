3ebc1028ed0b351d5be7471237b26800d245e987
Albert Mpepo <albertmpepo@gmail.com>
2023-11-02 21:28:56 +0200

    11. Size

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,15 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
+	size_t height = 0;
 
 	if (tree == NULL)
 		return (0);
 	
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		height = height + 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		height = height + 1 + binary_tree_height(tree->right);
 
-	if (l_height > r_height)
-		return (l_height + 1);
-	else
-		return (r_height + 1);
+	return (height);
 }
