451e0794c0039f7ea971a6def42c78e8955c86db
Albert Mpepo <albertmpepo@gmail.com>
2023-11-03 04:05:59 +0200

    2. Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,10 +11,20 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
 		return (NULL);
 
-	parent->right = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 
-	return (parent->right);
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+
+	parent->right = new_node;
+
+	return (new_node);
 }
