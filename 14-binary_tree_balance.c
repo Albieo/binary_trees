6fa2eae32d494db9f532d2e2e3bca91ed56be3ed
Albert Mpepo <albertmpepo@gmail.com>
2023-11-03 03:47:01 +0200

    14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,13 +11,14 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+		return (left_height - right_height);
+	}
+	return (0);
 }
 
 /**
