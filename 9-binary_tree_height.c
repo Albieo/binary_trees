dd0b1c1e3cb964186da80c54ac7af50dd743675c
Albert Mpepo <albertmpepo@gmail.com>
2023-11-03 03:27:17 +0200

    Test files removed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,18 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
+
 	if (tree)
 	{
-		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		if (tree->left != NULL)
+			left_height = 1 + binary_tree_height(tree->left);
+		else 
+			left_height = 0;
+
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
+		else
+			right_height = 0;
 		
 		if (left_height > right_height)
 			return (left_height);
