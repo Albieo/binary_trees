5b79af227a839a5672701c15dbc6726a56d093da
Albert Mpepo <albertmpepo@gmail.com>
2023-11-03 03:51:01 +0200

    16. Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -26,3 +26,18 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/**
+ * binary_tree_depth - a function that measures the depth of
+ * a node in a binary tree.
+ * @tree: a pointer to the root node of the tree to measure the depth.
+ *
+ * Return: the depth of tree, 0 if tree is NULL.
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree && tree->parent)
+		return (1 + binary_tree_depth(tree->parent));
+
+	return (0);
+}
